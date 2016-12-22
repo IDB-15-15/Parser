7f2d3391138ee7289f8aa88d9e6b457d1ef8a170
alexdanili <adm.minefun@yandex.ru>
2016-12-22 18:58:41 +0300

    add tree.h

---
+++ src/tree.h
@@ -0,0 +1,50 @@
+#ifndef tree
+#define tree
+#include <string>
+#include <vector>
+#include "boost/variant.hpp"
+
+#include <map>
+namespace Parser {
+        struct Tree{
+
+		class Tag;
+        class Text
+        {
+        public:
+            std::string value;
+            Tag* parent = nullptr;
+            Text();
+
+            ~Text() = default;
+
+            Text(std::string in_value){
+                this->value = in_value;
+            }
+        };
+
+        class Tag
+        {
+        public:
+            std::string name;
+            std::map <std::string,std::string> attributes;
+            std::vector<boost::variant<Tag,Text>> children;
+            boost::variant<Tag,Text>* parent = nullptr;
+
+            Tag(){}
+
+
+            Tag
+            (std::string _name,std::map<std::string,std::string> _attributes)
+            {
+                name = _name;
+                attributes = _attributes;
+            }
+
+            ~Tag() = default;
+        };
+        boost::variant<Tag,Text> root;
+        };
+    
+}
+#endif
