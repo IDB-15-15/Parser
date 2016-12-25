570c5e910b9dd07aff5f19041f86ca6050b927c2
alexdanili <adm.minefun@yandex.ru>
2016-12-26 01:32:03 +0300

    mian

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,54 @@
+#include "networklib.h"
+#include "network.h"
+#include <networkres.h>
+#include <boost/variant.hpp>
 #include <iostream>
 #include "parser.h"
-#include <boost/variant.hpp>
+
 
 void walk(boost::variant<Parser::Tree::Tag,Parser::Tree::Text> root){
     if(root.which() == 0){
         Parser::Tree::Tag tag = boost::get<Parser::Tree::Tag>(root);
-        if(tag.children.size() != 0){
-            std::cout<<tag.name<<"(";
-            typedef std::map<std::string, std::string>::const_iterator MapIterator;
+		if(tag.name == "li"){
+			Parser::Tree::Text t = tag.to_text();
+			std::cout<<std::endl;
+			std::cout.write(t.value,t.size);
+			std::cout<<std::endl;
+			std::cout<<std::endl;
+		}
+		std::cout<<"Tag : "<<tag.name<<"(";		
+		typedef std::map<std::string, std::string>::const_iterator MapIterator;
             for(MapIterator  iter = tag.attributes.begin(); iter != tag.attributes.end(); iter++){
                 std::cout<<iter->first<<"="<<iter->second<<" ";
             }
-            std::cout<<")"<<std::endl;
+			std::cout<<")"<<std::endl;
+			
+			
+        if(tag.children.size() != 0){        
+            
             for(int i = 0; i < tag.children.size(); i++){
                 boost::variant<Parser::Tree::Tag,Parser::Tree::Text> next(boost::get<Parser::Tree::Tag>(root).children[i]);
                 walk(next);
             }
+
         }
-        
+
     }else{
         Parser::Tree::Text text = boost::get<Parser::Tree::Text>(root);
-        std::cout<<text.value<<std::endl;
+		std::cout<<text.str<<std::endl;
     }
 
 }
 
 int main(){
-    std::shared_ptr<std::string> input = std::make_shared<std::string>("<!DOCTYPE HTML><html><head><title>KEKEKEKEEK</title></head><!--This is a comment. Comments are not displayed in the browser--><body text='aqua'><a href='http://ya.ru'>yandex</a><font size=\"10\">asda</font></body></html>");
-    Parser::Tree treer = Parser::parse(input);
+	setlocale (LC_ALL,"");
+	Network::NetworkRes res;
+	res = Network::give_result("file://C:/index.html");
+    Parser::Tree treer = Parser::parse(&res);
     Parser::Tree::Tag root = boost::get<Parser::Tree::Tag>(treer.root);
+	std::cout<<std::endl;
+	std::cout<<"this is walk"<<std::endl;
     walk(treer.root);
-    return 0;
+
+	return 0;
 }
